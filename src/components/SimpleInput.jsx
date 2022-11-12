7a8bc840dbffea2268cd78382abbc5037caa0e5b
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-11-11 22:20:04 -0500

    UseRef or every keystroke

--- src/components/SimpleInput.jsx
+++ src/components/SimpleInput.jsx
@@ -1,11 +1,23 @@
 
+import { useRef, useState } from "react"
 
 const SimpleInput=(props)=>{
+  const nameInputRef = useRef()
+  const [enteredName, setEnteredName] = useState('')
+  const nameInputChangeHandler = event =>{
+    setEnteredName(event.target.value)
+  }
+  const formSubmission = (e) =>{
+    e.preventDefault()
+    console.log(enteredName)
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue)
+  }
   return (
-    <form>
+    <form onSubmit={ formSubmission }>
       <div>
         <label htmlFor="name"> Your Name</label>
-        <input type="text"  />
+        <input ref={nameInputRef} type="text" onChange={ nameInputChangeHandler } />
       </div>
       <div>
         <button>Submit</button>
