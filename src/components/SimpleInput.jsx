a0904da535edefe6c7172e15b777528a4c8f7229
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-11-12 17:27:53 -0500

    error feedback practice

--- src/components/SimpleInput.jsx
+++ src/components/SimpleInput.jsx
@@ -4,11 +4,17 @@ import { useRef, useState } from "react"
 const SimpleInput=(props)=>{
   const nameInputRef = useRef()
   const [enteredName, setEnteredName] = useState('')
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
   const nameInputChangeHandler = event =>{
     setEnteredName(event.target.value)
   }
   const formSubmission = (e) =>{
     e.preventDefault()
+    if(enteredName.trim() === ''){
+      setEnteredNameIsValid(false)
+      return;
+    }
+    setEnteredNameIsValid(true)
     console.log(enteredName)
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue)
@@ -19,6 +25,7 @@ const SimpleInput=(props)=>{
         <label htmlFor="name"> Your Name</label>
         <input ref={nameInputRef} type="text" onChange={ nameInputChangeHandler } />
       </div>
+      {!enteredNameIsValid && <p>Name must not be empty.</p>}
       <div>
         <button>Submit</button>
       </div>
