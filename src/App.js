01cfe3ef564922562d5adc7c1f3835dd72d967b4
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-11-14 21:57:37 -0500

    Working with basic form comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import SimpleInput from "./components/SimpleInput";
+import BasicForm from "./components/BasicForm";
+// import SimpleInput from "./components/SimpleInput";
 
 const App =()=>{
   return (
     <>
     <h1>Hi Wubit!</h1>
-    <SimpleInput />
+    {/* <SimpleInput /> */}
+    <BasicForm />
     </>
   )
 }
