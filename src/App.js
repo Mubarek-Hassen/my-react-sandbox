817d783fb5c33c76261fb67794d631d3af2fdd6d
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-11-16 22:59:21 -0500

    useReducer practice with custom hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import BasicForm from "./components/BasicForm";
-// import SimpleInput from "./components/SimpleInput";
+// import BasicForm from "./components/BasicForm";
+import SimpleInput from "./components/SimpleInput";
 
 const App =()=>{
   return (
     <>
     <h1>Hi Wubit!</h1>
-    {/* <SimpleInput /> */}
-    <BasicForm />
+    <SimpleInput />
+    {/* <BasicForm /> */}
     </>
   )
 }
--- src/hooks/use-input.jsx
+++ src/hooks/use-input.jsx
@@ -1,27 +1,51 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: '',
+  isTouched: false
+}
+
+const inputStateReducer =(state, action)=>{
+  if (action.type === "INPUT"){
+    return {value: action.value, isTouched: state.isTouched}
+  }
+  if (action.type === "BLUR"){
+    return { isTouched: true, value: state.value}
+  }
+  if (action.type === 'RESET'){
+    return { value: '', isTouched: false}
+  }
+  return inputStateReducer
+}
 
 const useInput =(validateValue)=>{
-  const [enteredValue, setEnteredValue] = useState('');
-  const [isTouched, setIsTouched] = useState(false);
 
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+  const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState)
+
+  // const [enteredValue, setEnteredValue] = useState('');
+  // const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = event =>{
-    setEnteredValue(event.target.value)
+    dispatch({type: 'INPUT', value: event.target.value})
+    // setEnteredValue(event.target.value)
   }
 
   const inputBlurHandler = e =>{
-    setIsTouched(true)
+    dispatch({type: 'BLUR'})
+    // setIsTouched(true)
   }
 
   const reset =()=>{
-    setEnteredValue('')
-    setIsTouched(false)
+    dispatch({type: 'RESET'})
+    // setEnteredValue('')
+    // setIsTouched(false)
   }
   
   return {
-    value: enteredValue, hasError, valueChangeHandler, inputBlurHandler, isValid: valueIsValid, reset
+    value: inputState.value, hasError, valueChangeHandler, inputBlurHandler, isValid: valueIsValid, reset
   };
 }
 
